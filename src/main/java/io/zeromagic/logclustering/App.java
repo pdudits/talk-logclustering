1af32844c636f552ba514bfb40d1b746c81dff95
Patrik Dudits <patrik.dudits@payara.fish>
2025-03-29 16:17:09 +0100

    Nicer output

--- src/main/java/io/zeromagic/logclustering/App.java
+++ src/main/java/io/zeromagic/logclustering/App.java
@@ -8,6 +8,8 @@ import io.zeromagic.logclustering.simple.TermVector;
 import java.io.FileReader;
 import java.io.IOException;
 import java.nio.file.Path;
+import java.time.Duration;
+import java.time.Instant;
 
 /**
  * Hello world!
@@ -18,8 +20,12 @@ public class App {
         var output = Path.of("target/clusters/");
 
         try (var in = new FileReader(input.toFile())) {
-            var clustering = new NaiveClustering<TermVector>(TermVector::cosineDistance, 0.4);
+            var clustering = new NaiveClustering<TermVector>(TermVector::cosineDistance, 0.3);
+            var start = Instant.now();
             JsonArrayInput.process(in, e -> clustering.add(TermVector.of(e, Tokenizer.SIMPLE)));
+            var end = Instant.now();
+
+            System.out.format("Clustering took %s\n", Duration.between(start, end));
 
             new Report(clustering.getClusters()).report(output, 20, 0.2);
         }
--- src/main/java/io/zeromagic/logclustering/Report.java
+++ src/main/java/io/zeromagic/logclustering/Report.java
@@ -6,42 +6,90 @@ import io.zeromagic.logclustering.simple.TermVector;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Random;
 import java.util.stream.IntStream;
 
 public class Report {
     private final List<Cluster<TermVector>> clustering;
+    private final Random rand = new Random();
 
     public Report(List<Cluster<TermVector>> clustering) {
-        this.clustering = clustering;
+        this.clustering = new ArrayList<>(clustering);
+        Collections.sort(this.clustering, Comparator.comparingInt(c -> c.members().size()));
     }
 
     public void report(Path output, int maxExamples, double sampleRate) throws IOException {
-        Files.createDirectories(output);
-        var index = 0;
-        for (var cluster : clustering) {
-            //file name is 3-digit cluster size and then the index
-            var fileName = String.format("%04d-%03d.txt", cluster.members().size(), index++);
+        // create and output basic statistics such as:
+        // number of messages, number of clusters, percentile distribution of cluster sizes
 
-            try (var writer = Files.newBufferedWriter(output.resolve(fileName))) {
+        var totalMessages = clustering.stream().mapToInt(c -> c.members().size()).sum();
+        var totalClusters = clustering.size();
+        var percentiles = new int[10];
+        for (int i = 0; i < percentiles.length; i++) {
+            var index = (int) Math.round((i + 1) * 0.1 * (clustering.size() - 1));
+            percentiles[i] = clustering.get(index).members().size();
+        }
+
+        System.out.format("Total messages: %8d\n", totalMessages)
+                .format("Total clusters: %8d\n\n", totalClusters)
+                .append("| Percentile | Cluster Size |\n")
+                .append("|------------|--------------|\n");
+
+        for (int i = 0; i < percentiles.length; i++) {
+            System.out.format("| %9d%% | %12d |\n", (i + 1) * 10, percentiles[i]);
+        }
+
+        // creaete files with samples.
+        Files.createDirectories(output);
+        // delete any existing files
+        Files.list(output).forEach(f -> {
+            try {
+                Files.delete(f);
+            } catch (IOException e) {
+                throw new RuntimeException(e);
+            }
+        });
+        var index = 0;
+        var smallClustersFile = output.resolve("0000-small-clusters.txt");
+        try (var smallFilesWriter = Files.newBufferedWriter(smallClustersFile)) {
+            for (var cluster : clustering) {
                 // pick at most sampleRate..maxExamples random examples from the cluster
-                var random = new Random();
-                var indices = IntStream.generate(() -> random.nextInt(cluster.members().size()))
-                        .distinct()
-                        .limit(Math.max(1,Math.min(Math.round(cluster.members().size() * sampleRate), maxExamples)))
-                        .toArray();
-                Arrays.stream(indices).mapToObj(i -> cluster.members().get(i)).forEach(tv -> {
+                var samples = Math.max(1, Math.min(Math.round(cluster.members().size() * sampleRate), maxExamples));
+
+                var writer = smallFilesWriter;
+
+                if (samples > 3) {
+                    //file name is 3-digit cluster size and then the index
+                    var fileName = String.format("%04d-%03d.txt", cluster.members().size(), index);
+                    writer = Files.newBufferedWriter(output.resolve(fileName));
+                }
+                writeExamples(cluster, samples, writer, index);
+                if (writer != smallFilesWriter) {
+                    writer.close();
+                }
+                index++;
+            }
+        }
+
+    }
+
+    private void writeExamples(Cluster<TermVector> cluster, long samples, Appendable writer, int index) throws IOException {
+        writer.append("Cluster #%3d, size %6d\n-------------------------\n\n".formatted(index, cluster.members().size()));
+        IntStream.generate(() -> rand.nextInt(cluster.members().size()))
+                .distinct()
+                .limit(samples)
+                .mapToObj(i -> cluster.members().get(i))
+                .forEach(tv -> {
                     try {
                         write(writer, tv);
                     } catch (IOException e) {
                         throw new RuntimeException(e);
                     }
                 });
-
-            }
-        }
     }
 
     private void write(Appendable out, TermVector termVector) throws IOException {
@@ -49,7 +97,7 @@ public class Report {
         var meta = e.metadata();
         out.append(meta.get("Pod")).append(" ");
         out.append("[").append(meta.get("Timestamp")).append("] ")
-         .append(meta.get("Level")).append(" ")
+                .append(meta.get("Level")).append(" ")
                 .append(meta.get("LoggerName")).append(" : ")
                 .append(e.body()).append("\n");
         if (e.exception() != null) {
