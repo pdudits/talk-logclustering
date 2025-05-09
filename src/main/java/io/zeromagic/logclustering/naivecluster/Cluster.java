555eb4011ec3fc13e191168aec9ddd1323a3341a
Patrik Dudits <patrik.dudits@payara.fish>
2025-05-09 10:51:00 +0200

    Cleaner code in Cluster

--- src/main/java/io/zeromagic/logclustering/naivecluster/Cluster.java
+++ src/main/java/io/zeromagic/logclustering/naivecluster/Cluster.java
@@ -3,22 +3,20 @@ package io.zeromagic.logclustering.naivecluster;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
+import java.util.stream.DoubleStream;
 
-public record Cluster<T>(T leader, List<T> members) {
+public record Cluster<VEC>(VEC leader, List<VEC> members) {
     public static <T> Cluster<T> of(T leader) {
         return new Cluster<>(leader, new CopyOnWriteArrayList<>(List.of(leader)));
     }
 
     public record Stats(double average, double max, double stdDev) {}
 
-    public Stats distributionStats(Metric<T> metric) {
-        var distances = new ArrayList<Double>();
-        for (var member : members) {
-            distances.add(metric.distance(leader, member));
-        }
-        var average = distances.stream().mapToDouble(Double::doubleValue).average().orElse(0);
-        var max = distances.stream().mapToDouble(Double::doubleValue).max().orElse(0);
-        var stdDev = Math.sqrt(distances.stream().mapToDouble(d -> Math.pow(d - average, 2)).sum() / distances.size());
+    public Stats distributionStats(Metric<VEC> metric) {
+        var distances = members.stream().mapToDouble(m -> metric.distance(leader, m)).toArray();
+        var average = DoubleStream.of(distances).average().orElse(0);
+        var max = DoubleStream.of(distances).max().orElse(0);
+        var stdDev = Math.sqrt(DoubleStream.of(distances).map(d -> Math.pow(d - average, 2)).sum() / distances.length);
         return new Stats(average, max, stdDev);
     }
 }
